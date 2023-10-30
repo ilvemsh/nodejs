563cc0b78dce8bf0885615950f8063d6975c973c
ilvemsh <141834586+ilvemsh@users.noreply.github.com>
2023-10-30 16:09:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 const fs = require('fs');
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
